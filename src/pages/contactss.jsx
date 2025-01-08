fc6c72c0b6c47bec4881b868677ee40146fe0d50
yash49766 <yashkapadiya191@gmail.com>
2025-01-08 09:17:26 +0530

    mini changes

--- src/pages/contactss.jsx
+++ src/pages/contactss.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import {Box} from "@mui/material";
 import {Route, Routes} from "react-router-dom";
-import Contact from "../contact/contact";
-import AddTestimonial from "../contact/addtestimonial";
-import Testimonial from "../contact/testimonial";
+import Contact from "../component/contact/contact";
+import AddTestimonial from "../component/contact/addtestimonial";
+import Testimonial from "../component/contact/testimonial";
 
 function Contactss(props) {
     return (
